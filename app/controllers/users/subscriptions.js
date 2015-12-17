3b117c2ceb35b62098630d77516cbb46ab7631fe
omarteodoroalegre <alegreomarteodoro@gmail.com>
2015-12-16 21:31:54 -0300

    magazines management update

--- app/controllers/users/subscriptions.js
+++ app/controllers/users/subscriptions.js
@@ -10,7 +10,7 @@
 /* @ngInject */
 
 (function() {
-    module.exports = function($scope, $mdBottomSheet, $location, $mdToast, $mdDialog, subscriptionService) {
+    module.exports = function($scope, $mdBottomSheet, $location, $mdToast, $mdDialog, subscriptionService, collectionService) {
         $scope.subscriptions = [];
         $scope.loading = true;
         $scope.error = false;
@@ -53,10 +53,31 @@
         $scope.getAll = function() {
             subscriptionService.getAll()
                 .success(function(res){
-                    console.log(res);
                     $scope.subscriptions = res.subscriptions;
                     $scope.loading = false;
                     $scope.error = false;
+
+                    for(var i = 0; i < $scope.subscriptions.length; i++) {
+                        if($scope.subscriptions[i].owner != undefined) {
+                            var collection = $scope.subscriptions[i].owner.collections[0];
+                            var suscription = $scope.subscriptions[i];
+
+                            collectionService.get(collection)
+                                .success(function (res) {
+                                    suscription.owner.collections = [];
+                                    suscription.owner.collections.push(res.collections);
+                                })
+                                .error(function(res) {
+                                    $mdToast.show(
+                                        $mdToast.simple()
+                                            .content('Ha ocurrido un error intentalo nuevamente mas tarde.')
+                                            .position('top right')
+                                            .hideDelay(3000)
+                                    );
+                                    console.log(res);
+                                });
+                        }
+                    }
                 })
                 .error(function(){
                     $mdToast.show(
